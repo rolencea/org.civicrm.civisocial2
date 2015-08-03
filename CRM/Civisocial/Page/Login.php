4e2fb13089ec61a559b09d67b89af337c6d8c200
ayprograms <itsrolence@gmail.com>
2015-08-03 09:57:02 +0100

    created schema files and used method of DAO generation from civi-volunteer to generate DAO. Also packaging extension for demo

---
+++ CRM/Civisocial/Page/Login.php
@@ -0,0 +1,68 @@
+<?php
+
+require_once 'CRM/Core/Page.php';
+
+class CRM_Civisocial_Page_Login extends CRM_Core_Page {
+
+	function getBackendURI(){
+		$backendURI = NULL;
+		$path = CRM_Utils_System::currentPath();
+		if (false !== strpos($path, '..')) {
+	    	die ("SECURITY FATAL: the url can't contain '..'. Please report the issue on the forum at civicrm.org");
+	    }
+		$path = split('/',$path);
+
+		if(!CRM_Utils_Array::value(3,$path)){
+			die ("BACKEND ERROR: No backend found in request");
+		}
+		else{
+			$backend = CRM_Utils_Array::value(3,$path);
+			if("facebook"==$backend){
+				$enabled = civicrm_api3('setting', 'getvalue', array('group' => 'CiviSocial Account Credentials', 'name' => 'enable_facebook'));
+				if($enabled){
+					$facebook_client_id = civicrm_api3('setting', 'getvalue', array('group' => 'CiviSocial Account Credentials', 'name' => 'facebook_app_id'));
+					$backendURI = "https://www.facebook.com/dialog/oauth?";
+					$backendURI .= "client_id=".$facebook_client_id;
+					$backendURI .= "&redirect_uri=".$this->getRedirectURI("facebook");
+				}
+			}
+			else if("googleplus"==$backend){
+				$enabled = civicrm_api3('setting', 'getvalue', array('group' => 'CiviSocial Account Credentials', 'name' => 'enable_googleplus'));
+				if($enabled){
+					$googleplus_client_id = civicrm_api3('setting', 'getvalue', array('group' => 'CiviSocial Account Credentials', 'name' => 'google_plus_key'));
+					$backendURI = "https://accounts.google.com/o/oauth2/auth?";
+					$backendURI .= "client_id=".$googleplus_client_id;
+					$backendURI .= "&redirect_uri=".$this->getRedirectURI("googleplus");
+				}
+			}
+		}
+		return $backendURI;
+	}
+
+	function getRedirectURI($backend){
+		$redirectURI = NULL;
+		if(!$backend){
+			die ("BACKEND ERROR: No backend found in request");
+		}
+		else{
+			$redirectURI = rawurldecode(CRM_Utils_System::url("civicrm/civisocial/".$backend."callback", NULL, TRUE));
+		}
+		return $redirectURI;
+	}
+
+
+  function run() {
+  	$session = CRM_Core_Session::singleton();
+  	
+  	$redirectTo = $this->getBackendURI();
+
+  	//$session->set("userID", "2");
+
+  	if($redirectTo){
+    	return CRM_Utils_System::redirect($redirectTo);
+    }
+
+    $this->assign('currentTime', "Backend Not Supported");
+    parent::run();
+  }
+}
---
+++ sql/civisocial_db_uninstall.sql
@@ -0,0 +1,23 @@
+
+DROP TABLE IF EXISTS `civicrm_civisocial_autodm_log`;
+DROP TABLE IF EXISTS `civicrm_civisocial_autotweet_log`;
+DROP TABLE IF EXISTS `civicrm_civisocial_autotweet_recurring`;
+DROP TABLE IF EXISTS `civicrm_civisocial_autotweet_rss_feeds`;
+DROP TABLE IF EXISTS `civicrm_civisocial_autotweet_rss_tweets`;
+DROP TABLE IF EXISTS `civicrm_civisocial_autotweet_scheduled`;
+DROP TABLE IF EXISTS `civicrm_civisocial_dms`;
+DROP TABLE IF EXISTS `civicrm_civisocial_engagement_account`;
+DROP TABLE IF EXISTS `civicrm_civisocial_followers`;
+DROP TABLE IF EXISTS `civicrm_civisocial_follow_log`;
+DROP TABLE IF EXISTS `civicrm_civisocial_friends`;
+DROP TABLE IF EXISTS `civicrm_civisocial_leaders`;
+DROP TABLE IF EXISTS `civicrm_civisocial_tweets`;
+DROP TABLE IF EXISTS `civicrm_civisocial_tweet_mentions`;
+DROP TABLE IF EXISTS `civicrm_civisocial_tweet_retweets`;
+DROP TABLE IF EXISTS `civicrm_civisocial_tweet_tags`;
+DROP TABLE IF EXISTS `civicrm_civisocial_tweet_urls`;
+DROP TABLE IF EXISTS `civicrm_civisocial_users`;
+DROP TABLE IF EXISTS `civicrm_civisocial_user_tags`;
+DROP TABLE IF EXISTS `civicrm_civisocial_Twitter_Settings`;
+DROP TABLE IF EXISTS `civicrm_civisocial_faceboook_Settings`;
+
